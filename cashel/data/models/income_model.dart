4aa5e8646e1206ef38649ffa3a5736f743888596
Muhammad Nodi Satriyo Fabian <kususps33@gmail.com>
2026-04-29 10:01:34 +0700

    Delete cashel/data directory

--- cashel/data/models/income_model.dart
+++
@@ -1,6 +0,0 @@
-class IncomeModel {
-  final double value;
-  final String month;
-
-  IncomeModel({required this.value, required this.month});
-}
--- cashel/data/models/order_stats_model.dart
+++
@@ -1,6 +0,0 @@
-class OrderStatsModel {
-  final int newOrders;
-  final int pendingOrders;
-
-  OrderStatsModel({required this.newOrders, required this.pendingOrders});
-}
