18cee3422ddcce631306fd734291b691e97837ef
danisnuramelina24012007-source <danisnuramelina24012007@gmail.com>
2026-05-06 20:12:42 +0700

    update custom textfield

--- cashel/lib/core/constants/api_constants.dart
+++
@@ -1,7 +0,0 @@
-class ApiConstants {
-  // Cukup ganti di sini satu kali saja jika IP berubah
-  static const String baseUrl = "http:// 192.168.1.7/api_cashel";
-  
-  static const String loginEndpoint = "$baseUrl/login.php";
-  static const String registerEndpoint = "$baseUrl/register.php";
-}
--- cashel/lib/shared/widgets/custom_textfield.dart
+++ cashel/lib/shared/widgets/custom_textfield.dart
@@ -3,22 +3,23 @@ import 'package:flutter/material.dart';
 class CustomTextField extends StatelessWidget {
   final String hint;
   final bool isPassword;
+  final TextEditingController? controller; // Tambahkan baris ini
 
   const CustomTextField({
     super.key,
     required this.hint,
     this.isPassword = false,
+    this.controller, // Tambahkan baris ini
   });
 
   @override
   Widget build(BuildContext context) {
     return TextField(
+      controller: controller, // Hubungkan ke controller internal
       obscureText: isPassword,
       decoration: InputDecoration(
         hintText: hint,
-        border: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(10),
-        ),
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(12)),
       ),
     );
   }
